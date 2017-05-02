50c186a505c4c977addea7782ef4dedaf393e655
Peter Keating <peter@moov2.com>
2017-05-02 18:01:36 +0100

    Add Orchard 1.10.2

--- tasks/download.js
+++ tasks/download.js
@@ -17,6 +17,7 @@ module.exports = function(grunt) {
      * Collection of URLs to Orchard source code downloads.
      */
     var orchardDownloads = [
+        { version: '1.10.2', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.2.zip' },
         { version: '1.10.1', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.1.zip' },
         { version: '1.10', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.zip' },
         { version: '1.9.3', url: 'https://github.com/OrchardCMS/Orchard/archive/1.9.3.zip' },
