959a2487aba3da40352e726514630985001c58fb
Peter Keating <pete.keating@etchuk.com>
2023-03-23 09:37:43 +0000

    Update URLs to download source code

--- tasks/download.js
+++ tasks/download.js
@@ -17,17 +17,17 @@ module.exports = function(grunt) {
      * Collection of URLs to Orchard source code downloads.
      */
     var orchardDownloads = [
-        { version: '1.10.2', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.2.zip' },
-        { version: '1.10.1', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.1.zip' },
-        { version: '1.10', url: 'https://github.com/OrchardCMS/Orchard/archive/1.10.zip' },
-        { version: '1.9.3', url: 'https://github.com/OrchardCMS/Orchard/archive/1.9.3.zip' },
-        { version: '1.9.2', url: 'https://github.com/OrchardCMS/Orchard/archive/1.9.2.zip' },
-        { version: '1.9.1', url: 'https://github.com/OrchardCMS/Orchard/archive/1.9.1.zip' },
-        { version: '1.9', url: 'https://github.com/OrchardCMS/Orchard/archive/1.9.zip' },
-        { version: '1.8.2', url: 'https://github.com/OrchardCMS/Orchard/archive/1.8.2.zip' },
-        { version: '1.8.1', url: 'https://github.com/OrchardCMS/Orchard/archive/1.8.1.zip' },
-        { version: '1.7.3', url: 'https://github.com/OrchardCMS/Orchard/archive/1.7.3.zip' },
-        { version: '1.7.1', url: 'https://github.com/OrchardCMS/Orchard/archive/1.7.1.zip' }
+        { version: '1.10.2', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.10.2.zip' },
+        { version: '1.10.1', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.10.1.zip' },
+        { version: '1.10', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.10.zip' },
+        { version: '1.9.3', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.9.3.zip' },
+        { version: '1.9.2', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.9.2.zip' },
+        { version: '1.9.1', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.9.1.zip' },
+        { version: '1.9', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.9.zip' },
+        { version: '1.8.2', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.8.2.zip' },
+        { version: '1.8.1', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.8.1.zip' },
+        { version: '1.7.3', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.7.3.zip' },
+        { version: '1.7.1', url: 'https://github.com/OrchardCMS/Orchard/archive/refs/tags/1.7.1.zip' }
     ];
 
     var helpers = {
